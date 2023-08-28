fcc3b1f782c6cb9a3f2b92e42aa5b1e4bd66a7cc
mokwar <mokwar7@gmail.com>
2023-08-28 13:03:08 +0300

    minor fix

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -8,7 +8,7 @@ function Techs() {
             <span className='techs__line'></span>
             <div className='techs__container-about'>
                 <h3 className='techs__header-about'>
-                    7 Технологий
+                    7 технологий
                 </h3>
                 <p className='techs__text'>
                     На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.
